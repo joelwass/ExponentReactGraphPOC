dc9f0aa20c7ad120b6681575cca552dc82949d6b
Joel Wasserman <joel.wasserman8@gmail.com>
2016-06-28 14:12:12 -0700

    Added in font awesome icons to top right, chat and battery icons

--- HomePageTopBar/TopBar.js
+++ HomePageTopBar/TopBar.js
@@ -5,14 +5,25 @@ import {
   StyleSheet,
   Image,
 } from 'react-native';
+import { Font } from 'exponent';
 
 export default class TopBar extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      assetsLoaded: false,
       childName: this.props.childName,
     };
+  }
+
+  async componentWillMount() {
+    // Load mandatory assets
+    await Font.loadAsync({
+      awesome: 'https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf',
+    });
+
+    this.setState({assetsLoaded: true});
   }
 
   render() {
@@ -20,12 +31,34 @@ export default class TopBar extends React.Component {
       <View style={styles.topBar}>
         <Image style={styles.childImage} source={require('./childImage.png')} />
         <Text style={styles.childName}>{this.state.childName}</Text>
+        <Text style={[styles.battery, { ...Font.style('awesome'), fontSize: 28 }]}>{'\uf240'}</Text>
+        <Text style={[styles.intercomChat, { ...Font.style('awesome'), fontSize: 28 }]}>{'\uf0e5'}</Text>
       </View>
+    );
+  }
+
+  _renderIcon(name, isSelected) {
+    return (
+      <FontAwesomeIcon
+        name={name}
+        size={32}
+        color={isSelected ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
     );
   }
 }
 
 let styles = StyleSheet.create({
+  battery: {
+    marginTop: 6,
+    marginLeft: 186,
+    marginRight: 22,
+    color: 'grey',
+  },
+  intercomChat: {
+    marginTop: 6,
+    color: 'grey',
+  },
   topBar: {
     marginTop: 15,
     width: 320,
